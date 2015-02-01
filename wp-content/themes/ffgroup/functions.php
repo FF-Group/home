9e2f687801de36dcc5b23d332233eff11fd5882d
FF-Group <dammit90@gmail.com>
2015-02-01 20:57:25 +0200

    dfgdfgdfg

--- wp-content/themes/ffgroup/functions.php
+++ wp-content/themes/ffgroup/functions.php
@@ -7,7 +7,9 @@ register_nav_menu( 'ff-menu', 'Main Menu' );
 }
 
 function wp_enqueue_theme_scrupts(){
+	
+	wp_enqueue_script( 'jquery' );
 	wp_enqueue_script( 'jquery-ui-sortable' );
 }
-add_action('wp_enqueue_scripts', 'wp_enqueue_theme_scrupts');
+add_action('admin_enqueue_scripts', 'wp_enqueue_theme_scrupts');
 
