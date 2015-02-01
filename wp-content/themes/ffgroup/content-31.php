e78618702256a93546f5dd6a0af75353de48db1e
FF-Group <dammit90@gmail.com>
2015-02-01 13:58:19 +0200

    fix

---
+++ wp-content/themes/ffgroup/content-31.php
@@ -0,0 +1,4 @@
+<div <?php post_class('panel'); ?>  id="post-<?php the_ID(); ?>">
+                    <h2><?php the_title(); ?></h2>
+                    <?php echo do_shortcode('[huge_it_gallery id=”1″]'); ?>
+                    </div> <!-- end of contact us -->
