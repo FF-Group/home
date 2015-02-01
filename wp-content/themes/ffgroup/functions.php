c3686a1d8b7fe97386aeb2001fca5b29d45c61ad
FF-Group <dammit90@gmail.com>
2015-02-01 19:06:56 +0200

    add sortable script

--- wp-content/themes/ffgroup/functions.php
+++ wp-content/themes/ffgroup/functions.php
@@ -6,3 +6,8 @@ if ( function_exists( 'register_nav_menu' ) ) {
 register_nav_menu( 'ff-menu', 'Main Menu' );
 }
 
+function wp_enqueue_theme_scrupts(){
+	wp_enqueue_script( 'jquery-ui-sortable' );
+}
+add_action('wp_enqueue_scripts', 'wp_enqueue_theme_scrupts');
+
