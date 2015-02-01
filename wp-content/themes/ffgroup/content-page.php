034111e58556a15bbfb92b8cf2cd8b81fa8f60d0
FF-Group <dammit90@gmail.com>
2015-02-01 12:43:17 +0200

    Обнуление

--- wp-content/themes/ffgroup/content-page.php
+++
@@ -1,5 +0,0 @@
-
-<div <?php post_class('panel'); ?>  id="post-<?php the_ID(); ?>">
-                    <h2><?php the_title(); ?></h2>
-                    <?php the_content(); ?>
-                    </div> <!-- end of contact us -->
--- wp-content/themes/ffgroup/content.php
+++
@@ -1,6 +0,0 @@
-
-<div class="panel" id="post-<?php the_ID(); ?>">
-                    <h2><?php echo get_the_title(); ?></h2>
-                    <?php echo get_the_content(); ?>
-                    </div> <!-- end of contact us -->
-                    
