71bbd9c1482aa4fdb570aa8ae7a60e28610bcaef
FF-Group <dammit90@gmail.com>
2015-02-01 22:29:00 +0200

    fixportfolio

--- wp-content/themes/ffgroup/functions.php
+++ wp-content/themes/ffgroup/functions.php
@@ -8,9 +8,7 @@ register_nav_menu( 'ff-menu', 'Main Menu' );
 
 function wp_enqueue_theme_scrupts(){
 	
-	wp_enqueue_script( 'jquery' );
-	wp_enqueue_script( 'jquery-ui-sortable' );
-	wp_enqueue_script( 'jquery-ui' );
+	wp_enqueue_script( 'ui', get_template_directory_uri(). '/js/jquery-ui.min.js', array('jquery') );
 }
 add_action('admin_enqueue_scripts', 'wp_enqueue_theme_scrupts');
 
